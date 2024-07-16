46a4843b7df89656052b110460fa9d3c1698e2ec
peace1nmind <aont95@naver.com>
2024-07-16 15:10:29 +0900

    Thyemleaf if, each, header, footer

--- src/main/java/com/peace1nmind/d0716/controller/JpaController.java
+++ src/main/java/com/peace1nmind/d0716/controller/JpaController.java
@@ -2,12 +2,15 @@ package com.peace1nmind.d0716.controller;
 
 import com.peace1nmind.d0716.entity.Member;
 import com.peace1nmind.d0716.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.net.http.HttpRequest;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -34,5 +37,50 @@ public class JpaController {
         model.addAttribute("member", member);
 
         return "info";
+    }
+
+
+    @GetMapping("loginOk")
+    public String loginOk(HttpServletRequest request, Model model) {
+
+        model.addAttribute("id", request.getParameter("id"));
+        model.addAttribute("pw", request.getParameter("pw"));
+
+        return "loginOk";
+    }
+
+
+    @GetMapping("/ifTest")
+    public String ifTest(Model model) {
+
+        Optional<Member> memberOptional = memberRepository.findById(1L);
+        Member member = memberOptional.get();
+
+        model.addAttribute("member", member);
+
+        return "ifTest";
+    }
+
+
+    // 모든 회원 리스트
+    @GetMapping("/members")
+    public String members(Model model) {
+
+        List<Member> memberList = memberRepository.findAll();
+        model.addAttribute("memberList", memberList);
+
+        return "eachTest";
+    }
+
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "layout";
+    }
+
+
+    @GetMapping("/content")
+    public String content() {
+        return "content";
     }
 }
